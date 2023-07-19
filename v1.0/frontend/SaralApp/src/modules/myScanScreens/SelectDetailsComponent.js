5f649a261f8106b026735edb107609ca8bd25033
anupAppzui <anup.nair@appzui.com>
2023-07-19 22:04:29 +0530

    holiday label fix

--- v1.0/frontend/SaralApp/src/modules/myScanScreens/SelectDetailsComponent.js
+++ v1.0/frontend/SaralApp/src/modules/myScanScreens/SelectDetailsComponent.js
@@ -879,7 +879,7 @@ dispatchStudentExamData(payload){
             if (valid) {
 
                 if (subjectsData[subIndex] === 'attendance' && selectedDayType === 'Holiday') {
-                    this.callCustomModal(Strings.message_text, "As today is a holiday, the attendance will be recorded and stored as a holiday", false, this.navBack);
+                    this.callCustomModal(Strings.message_text, "By selecting the 'Holiday' type, the attendance will be recorded and marked as a holiday entry.", false, this.navBack);
                     this.setState({
                         isLoading: false
                     });
